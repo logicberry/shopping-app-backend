50b4c036e19035d727f3b27fc763c3c9c9fd38af
Malik Rasaq <malikrasaq@gmail.com>
2023-11-26 00:02:36 +0100

    fix: env issues

--- src/main/java/com/codegenius/shop/ShoppingAppApplication.java
+++ src/main/java/com/codegenius/shop/ShoppingAppApplication.java
@@ -2,8 +2,10 @@ package com.codegenius.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.codegenius.shop")
 public class ShoppingAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingAppApplication.class, args);
--- src/main/java/com/codegenius/shop/utils/CloudinaryConfig.java
+++ src/main/java/com/codegenius/shop/utils/CloudinaryConfig.java
@@ -3,20 +3,18 @@ package com.codegenius.shop.utils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.cloudinary.Cloudinary;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${CLOUDINARY_URL}")
+    private String cloudinaryUrl;
+
     @Bean
     public Cloudinary cloudinary() {
-        // Load Cloudinary credentials from environment variables
-        Dotenv dotenv = Dotenv.load();
-        String cloudinaryUrl = dotenv.get("CLOUDINARY_URL");
-
         // Create and configure a Cloudinary instance
-        Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
-
-        return cloudinary;
+        return new Cloudinary(cloudinaryUrl);
     }
+
 }
